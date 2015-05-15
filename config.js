a248ba71688470c544467f43d97d39b09686e1b8
German Attanasio Ruiz <germanattanasio@gmail.com>
2015-05-15 17:19:17 -0400

    Update config.js
    update twitter config

--- config.js
+++ config.js
@@ -2,24 +2,9 @@
 
 module.exports = {
   twitter: [{
-    consumer_key: 'PT5g5KKLcGVlUpOhSDzgM4HfR',
-    consumer_secret: 'GweDOnZZUX1VlcBkvoaDQJaC7VCXkgSdopTt4mDK7O93avBDvW',
-    access_token_key: '17887906-bXpV8Va1wlZFqEp3BQLuhg9FwaALIVvXPxB91BZJ4',
-    access_token_secret: 'fYzNpmOaC3QoNL22cytgghvh7MuvOwPT1CYrIggMLsA7V'
-  }, {
-    consumer_key: 'dG8X0N8iBrR6QYon6njm0Q',
-    consumer_secret: 'uNgYsug2jt7Vfe6t8YcdRe0Hi3r90Mkspf8VHY2oLg',
-    access_token_key: '2416405548-BzTMqxLFyyvF5L43chHaePWh3N3zCWYKvAdXD2c',
-    access_token_secret: '6HMO68wFEtuJS3jZI8vkhXOSdVN356rw0idTyZuMYQcry'
-  }, {
-    consumer_key: 'JjjtepInW4egPNgLeUYmQ',
-    consumer_secret: 'HIOlFJXuo5h93jUkEZwqOoFnTx1Q504PZB1qZMqU',
-    access_token_key: '27742725-0poUezkRPPwjpfGvrs2hZGUByBxy2r3cVGPHrT1Kg',
-    access_token_secret: 'VMeNMmOtIvrrxsjxpn491CyUqtXXL1Fi0xBgWdDo'
-  }, {
-    consumer_key: 'j7QZPwL54cRcZy0YpWSK1g',
-    consumer_secret: 'ZsPqjPP6yJORFLLp5fPfTRBMqYRnmWisg29l25wwWs',
-    access_token_key: '152919543-IqQGLhP7TKJWYn0JR0ejokfvbry7er7CEgtMzJAr',
-    access_token_secret: 'bPicRVweZwzLAGBOpGOapzcw2nOvm62v2ns3BySE0SE'
+    consumer_key: 'G16KQMbKx1Owm8jbhalCqDcCE',
+    consumer_secret: 'YyCUStyKozBKJMcWrvDXFTVQdr6MYUsIPGY6VfnGcB0LRXw8Od',
+    access_token_key: '14578997-xlgEOLgVz3rinxXneqAFUUdsXk7eitbw2B9OdpH8N',
+    access_token_secret: 'nUp5ax8T1SMsPIl4pcsymBQr3ebvU8hrdxY9HV7iW6VUh'
   }]
 };
