f634bbec409cc9f204fc1265894e17dd59f4ea04
German Attanasio Ruiz <germanattanasio@gmail.com>
2015-07-09 01:38:27 -0400

    Update config.js
    update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@
 
 module.exports = {
   twitter: [{
-    consumer_key: 'G16KQMbKx1Owm8jbhalCqDcCE',
-    consumer_secret: 'YyCUStyKozBKJMcWrvDXFTVQdr6MYUsIPGY6VfnGcB0LRXw8Od',
+    consumer_key: 'e39o29ZT8FcSJHk18KPvGTFmA',
+    consumer_secret: 'U9kIEsueV6GUVIRS9EyuN4CJZ34CVXNIw2WrSdq2XZPWx12kAf',
     access_token_key: '14578997-xlgEOLgVz3rinxXneqAFUUdsXk7eitbw2B9OdpH8N',
     access_token_secret: 'nUp5ax8T1SMsPIl4pcsymBQr3ebvU8hrdxY9HV7iW6VUh'
   }]
